56520e03c0998b0015a58d58a129c1a8db62ba0d
shaheen ansari <shaheenansari6556@gmail.com>
2025-06-22 09:21:19 +0530

    update sh file

--- start.sh
+++ start.sh
@@ -12,15 +12,22 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 #run one-time
-python manage.py createsuperuser --noinput \
-  --username shaheenansari1906@gmail.com \
-  --email shaheenansari1906@gmail.com
+# python manage.py createsuperuser --noinput \
+#   --username shaheenansari1906@gmail.com \
+#   --email shaheenansari1906@gmail.com
+
+# echo "from django.contrib.auth import get_user_model; \
+#    User = get_user_model(); \
+#    user = User.objects.get(username='shaheenansari1906@gmail.com'); \
+#    user.set_password('Adminpass@123'); \
+#    user.save()" \ 
+#    | python manage.py shell
 
 echo "from django.contrib.auth import get_user_model; \
-   User = get_user_model(); \
-   user = User.objects.get(username='shaheenansari1906@gmail.com'); \
-   user.set_password('Adminpass@123'); \
-   user.save()" \ 
-   | python manage.py shell
+  User = get_user_model(); \
+  user = User.objects.get(username='shaheenansari1906@gmail.com'); \
+  user.set_password('adminpass@123'); \
+  user.save()" \
+  | python manage.py shell
 
 gunicorn task_management.wsgi.application --bind 0.0.0.0$PORT
