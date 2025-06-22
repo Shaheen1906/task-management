583d7c795eb71ea512ab917bf979081e31dd7fd6
shaheen ansari <shaheenansari6556@gmail.com>
2025-06-22 09:07:59 +0530

    added requirements.txt and important settings

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+
+pip install -r requirements.txt
+
+
+python manage.py makemigrations --noinput
+
+python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
+
+#run one-time
+python manage.py createsuperuser --noinput \
+  --username shaheenansari1906@gmail.com \
+  --email shaheenansari1906@gmail.com
+
+echo "from django.contrib.auth import get_user_model; \
+   User = get_user_model(); \
+   user = User.objects.get(username='shaheenansari1906@gmail.com'); \
+   user.set_password('Adminpass@123'); \
+   user.save()" \ 
+   | python manage.py shell
+
+gunicorn task_management.wsgi.application --bind 0.0.0.0$PORT
--- task_management/settings.py
+++ task_management/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-zjor$i2@9i@lhxpr6tdcmrdu2fq*ahv39yw0r=rho4u6v4y2!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,11 +79,17 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
@@ -121,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
